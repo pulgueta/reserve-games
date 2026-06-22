072e6d52146b740e71021414836607890a5dbc84
pulgueta <retardix456@gmail.com>
2026-06-21 23:41:55 -0500

    chore: remove drizzle, sentry, and demo scaffolding

--- src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-
-import { env } from "@/env";
-import * as schema from "./schema.ts";
-
-export const db = drizzle(env.DATABASE_URL, { schema });
--- src/integrations/clerk/header-user.tsx
+++
@@ -1,7 +0,0 @@
-import { SignInButton, UserButton, useAuth } from "@clerk/react";
-
-export default function HeaderUser() {
-  const { isSignedIn } = useAuth();
-
-  return isSignedIn ? <UserButton /> : <SignInButton />;
-}
